9dfb378d55b538cd0123eda1424d2efe59977934
anasjaved498-svg <anasjaved498@gmail.com>
2026-09-22 12:32:20 -0700

    Update layout.tsx

--- app/quran/(public)/layout.tsx
+++ app/quran/(public)/layout.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import AnnouncementPopup from "@/components/AnnouncementPopup";
 
 export default function PublicQuranLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
+      <AnnouncementPopup />
       <main className="flex-1">{children}</main>
       <Footer />
     </>
